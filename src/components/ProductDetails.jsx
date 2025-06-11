00547a7dba837c1fe54d0b3b9db6da5c9f3fbab7
KalebMorton <kmorton7714@gmail.com>
2025-06-10 20:01:01 -0600

    set up Product Details with placeholder values

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,4 +1,13 @@
-export default function ProductDetails(){
+import { useEffect, useState } from "react"
+
+export default function ProductDetails(passedData){
+
+    const [singleProduct, setSingleProduct] = useState(null)
+
+    async function getSingleProdct() {
+        const response = await fetch(`url goes here ${passedData}`)
+    }
+
     return (
         <>
             <h2>ProductDetails</h2>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,7 +1,37 @@
-export default function ProductList(){
+import { useEffect, useState } from "react";
+import ProductDetails from "./ProductDetails"
+
+export default function ProductList(passedData){
+
+    const [wholeList, setWholeList] = useState(null)
+    const [singleProductId, setSingleProductId] = useState (null)
+
+    async function getProducts() {
+        const response = await fetch("url goes here")
+        const rawData = await response.json()
+
+        setWholeList(rawData.map((item) => {
+            return <div id="listing">
+                <h3>{item.dataplaceholder1}</h3>
+                <p>{item.dataplaceholder2}</p>
+                <p>{item.dataplaceholder3}</p>
+                <button onClick={handleClick} id={item.placeHolderId}>More Info</button>
+            </div>
+        }))
+    }
+
+    function handleClick(event){
+        setSingleProductId(event.tartget.id)
+    }
+
+    useEffect(() => {
+       //getProducts() 
+    },[]);
+
     return (
         <>
-            <h2>ProductList</h2>
+            <h2>Product List</h2>
+            {singleProductId ? <ProductDetails ProductId={singleProductId} setSingleProductId={setSingleProductId}/> : (wholeList && <>{wholeList}</>)}
         </>
     )
 }

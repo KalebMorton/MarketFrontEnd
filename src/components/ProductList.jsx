f9e352e531b422a1155e2c5334c5dbed196b9914
KalebMorton <kmorton7714@gmail.com>
2025-06-12 17:31:53 -0600

    completed product list and product details with placholder varibles

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -7,17 +7,22 @@ export default function ProductList(passedData){
     const [singleProductId, setSingleProductId] = useState (null)
 
     async function getProducts() {
-        const response = await fetch("url goes here")
-        const rawData = await response.json()
+        try {
+            const response = await fetch("url goes here")
+            const rawData = await response.json()
 
-        setWholeList(rawData.map((item) => {
-            return <div id="listing">
-                <h3>{item.dataplaceholder1}</h3>
-                <p>{item.dataplaceholder2}</p>
-                <p>{item.dataplaceholder3}</p>
-                <button onClick={handleClick} id={item.placeHolderId}>More Info</button>
-            </div>
-        }))
+            setWholeList(rawData.map((item) => {
+                return <div id="listing">
+                    <h3>{item.dataplaceholder1}</h3>
+                    <p>{item.dataplaceholder2}</p>
+                    <p>{item.dataplaceholder3}</p>
+                    <button onClick={handleClick} id={item.placeHolderId}>More Info</button>
+                </div>
+            }))
+        } catch (error) {
+            console.log(`!ERROR! ${error}`)
+        }
+        
     }
 
     function handleClick(event){
@@ -31,7 +36,10 @@ export default function ProductList(passedData){
     return (
         <>
             <h2>Product List</h2>
-            {singleProductId ? <ProductDetails ProductId={singleProductId} setSingleProductId={setSingleProductId}/> : (wholeList && <>{wholeList}</>)}
+            {
+                //if the singleProductId is null, render wholeList, else render singleProductId
+            }
+            {singleProductId ? <ProductDetails productId={singleProductId} setSingleProductId={setSingleProductId}/> : (wholeList && <>{wholeList}</>)}
         </>
     )
 }

e7cc27b746729390e858d13f00f82d39e2196d3f
Lyndsey <lyndseymsmith44@gmail.com>
2025-06-12 20:51:17 -0400

    added rubber duck icon, title

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,37 +1,38 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function ProductDetails(passedData){
+export default function ProductDetails(passedData) {
+  const [singleProduct, setSingleProduct] = useState(null);
 
-    const [singleProduct, setSingleProduct] = useState(null)
+  async function getSingleProdct() {
+    try {
+      const response = await fetch(`url goes here/${passedData.productId}`);
+      const rawData = await response.json();
 
-    async function getSingleProdct() {
-        try {
-            const response = await fetch(`url goes here/${passedData.productId}`)
-            const rawData = await response.json()
-
-            setSingleProduct(rawData)
-        } catch (error) {
-            console.log(`!ERROR! ${error}`)
-        }
+      setSingleProduct(rawData);
+    } catch (error) {
+      console.log(`!ERROR! ${error}`);
     }
+  }
 
-    useEffect(() => {
-       //getSingleProdct() 
-    },[]);
+  useEffect(() => {
+    //getSingleProdct()
+  }, []);
 
-    function backButton(){
-        passedData.setSingleProductId(null)
-    }
+  function backButton() {
+    passedData.setSingleProductId(null);
+  }
 
-    return (
-        <>
-            <h2>ProductDetails</h2>
-            {singleProduct && <div>
-                <h3>{singleProduct.dataplaceholder1}</h3>
-                <p>{singleProduct.dataplaceholder2}</p>
-                <p>{singleProduct.dataplaceholder3}</p>
-                <button onClick={backButton}>Go Back</button>
-            </div>}
-        </>
-    )
+  return (
+    <>
+      <h2>ProductDetails</h2>
+      {singleProduct && (
+        <div>
+          <h3>{singleProduct.dataplaceholder1}</h3>
+          <p>{singleProduct.dataplaceholder2}</p>
+          <p>{singleProduct.dataplaceholder3}</p>
+          <button onClick={backButton}>Go Back</button>
+        </div>
+      )}
+    </>
+  );
 }

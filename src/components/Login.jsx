364aad78d6f0af3a6d12bb04ec6e3779fd8835a4
Danielle <dani.camp1125@gmail.com>
2025-06-23 16:18:13 -0400

    object deconstruction for login--Dani

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,11 @@
-export default function Login() {
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
+export default function Login({email, setEmail, password, setPassword, token, setToken}) {
+  const [error, setError] = useState(null)
+  const [success, setSuccess] = useState(null)
+  const navigate= useNavigate()
+  
   return (
     <div className='container'>
       <h2>Login</h2>
